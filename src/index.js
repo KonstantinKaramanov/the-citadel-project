1a4d27c2fd7083f37a91f5693dd25758d56e0f41
Konstantin <flexaaa@abv.bg>
2025-04-12 20:45:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { FirebaseAppProvider } from 'reactfire';
 import firebase from 'firebase/compat/app';
 
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBo-VnJr52CECHIO0_r4HQXh_hQDcWBGkA",
-  authDomain: "the-citadel-project.firebaseapp.com",
-  databaseURL: "https://the-citadel-project-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "the-citadel-project",
-  storageBucket: "the-citadel-project.appspot.com",
-  messagingSenderId: "519832819116",
-  appId: "1:519832819116:web:35b8d2cdf04d08c5809609"
-};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
